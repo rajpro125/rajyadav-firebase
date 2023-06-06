b5934c760f689b166663623ab94320449387400d
rajpro125 <134783220+rajpro125@users.noreply.github.com>
2023-06-06 09:30:34 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/vivatech/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vivatech/MainActivity.kt
@@ -2,5 +2,6 @@ package com.example.vivatech
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterActivity {
+
 }
